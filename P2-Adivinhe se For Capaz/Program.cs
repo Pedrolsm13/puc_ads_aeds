150caceac8b19d6ac987cb8761e03cc2da73ef81
Pedro Lucas <plserramoura@gmail.com>
2023-12-01 18:23:41 -0300

    prova 2

---
+++ P2-Adivinhe se For Capaz/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program{
+    public static void Main(string [] args){
+        int n = 1;
+        while(n != 0){
+            n = int.Parse(Console.ReadLine());
+            if(n == 0)
+                break;
+            Stack<string> pilha = new Stack<string>();
+            Queue<string> fila = new Queue<string>();
+            bool f = false;
+            bool l = false;
+            bool p = false; 
+            for(int i = 0; i < n; i++){
+                string [] linha = Console.ReadLine().Split(' ');
+                if(linha[0] == "I"){
+                    pilha.Push(linha[1]);
+                    fila.Enqueue(linha[1]);
+                }
+                else if(linha[0] == "R"){
+                    if(linha[1] == pilha.Peek()){
+                        pilha.Pop();
+                        fila.Dequeue();
+                        f = false;
+                        l = false;
+                        p = true;
+                    }
+                    else if(linha[1] == fila.Peek()){
+                        pilha.Pop();
+                        fila.Dequeue();
+                        f = true;
+                        l = false;
+                        p = false;
+                    }
+                    else{
+                        f = false;
+                        l = true;
+                        p = false;
+                    }
+                }
+            }
+            if(f)
+                Console.WriteLine("queue");
+            else if(l)
+                Console.WriteLine("list");
+            else if(p)
+                Console.WriteLine("stack");
+        }
+    }
+}
---
+++ P2-Star Trek/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program{
+    static void Main(string[] args){
+        int v = int.Parse(Console.ReadLine());
+        Personagem[] Pers_vet = new Personagem[v];
+        for(int i = 0; i < v; i++){
+            string linha = Console.ReadLine();
+            Pers_vet[i] = new Personagem(linha);
+        }
+        bool troca = true;
+        for(int i = 0; i < Pers_vet.Length-1 && troca; i++){
+            troca = false;
+            for(int j = 0; j < Pers_vet.Length-(i+1); j++){
+                if(Pers_vet[j].nome.CompareTo(Pers_vet[j+1].nome) > 0){
+                    Personagem aux = Pers_vet[j];
+                    Pers_vet[j] = Pers_vet[j+1];
+                    Pers_vet[j+1] = aux;
+                    troca = true;
+                }
+            }
+        }
+        for(int k = 0; k < Pers_vet.Length; k++){
+            Console.WriteLine($"{Pers_vet[k].nome}-{Pers_vet[k].raca}-{Pers_vet[k].serie}");
+        }
+    }
+}
+
+class Personagem{
+    public string nome{get; private set;}
+    public string raca{get; private set;}
+    public string serie{get; private set;}
+
+    public Personagem(string linha){
+        string [] vet = linha.Split(';');
+        this.nome = vet[0];
+        this.raca = vet[1];
+        this.serie = vet[2];
+    }
+}
